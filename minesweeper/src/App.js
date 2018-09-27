a7c790934eb0a845a08ed37777102ecd912ea314
Gabe Oleinik <gomusic14@gmail.com>
2018-09-27 13:44:54 -0400

    Html and css

---
+++ minesweeper/src/App.js
@@ -0,0 +1,103 @@
+import React, { Component } from "react"
+import logo from "./logo.svg"
+import "./App.css"
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          Mine Sweeper
+        </header>
+        <main>
+          <table>
+            <tbody>
+              <tr>
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+              </tr>
+              <tr>
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+              </tr>
+              <tr>
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+              </tr>
+              <tr>
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+              </tr>
+              <tr>
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+              </tr>
+              <tr>
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+              </tr>
+              <tr>
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+              </tr>
+              <tr>
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+              </tr>
+            </tbody>
+          </table>
+        </main>
+      </div>
+    )
+  }
+}
+
+export default App
